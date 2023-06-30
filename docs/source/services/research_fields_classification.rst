070fc7ec9d9811e83e3d705e175e3336ab5d15e8
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-06-30 09:56:21 +0000

    fix research field classifier input variable

--- docs/source/services/research_fields_classification.rst
+++ docs/source/services/research_fields_classification.rst
@@ -6,7 +6,7 @@ Overview
 
 This research field classification service aims to predict the corresponding research fields for given papers.
 It is designed to assist contributors who may not be familiar with the extensive research field taxonomy present
-in the ORKG, enabling them to save significant amounts of time. By analysing the abstract of a paper, the service
+in the ORKG, enabling them to save significant amounts of time. By analysing the title and abstract of a paper, the service
 suggests potential research fields that align with the content. This empowers authors to effortlessly select an
 appropriate research field without requiring in-depth knowledge of the research field taxonomy.
 
@@ -18,7 +18,7 @@ Usage
     from orkgnlp.annotation import ResearchFieldClassifier
 
     rf_classifier = ResearchFieldClassifier() # This will also download the required model files.
-    rfs = rf_classifier(abstract='Your paper abstract here', top_n=10)
+    rfs = rf_classifier(raw_input='Your paper combined title with abstract here', top_n=10)
     print(rfs)
 
 and the output has the following schema:
--- tests/annotation/test_rfclf_classifier.py
+++ tests/annotation/test_rfclf_classifier.py
@@ -7,7 +7,7 @@ from orkgnlp.annotation import ResearchFieldClassifier
 class TestResearchFieldClassifier(TestCase):
     def setUp(self):
         self.classifier = ResearchFieldClassifier(force_download=False)
-        self.abstract = """Understanding of cybersecurity threat landscape especially information about threat
+        self.raw_input = """Understanding of cybersecurity threat landscape especially information about threat
                            actor is a challenging task as these information are usually hidden and scattered.
                            The online news had became one of the popular and important source of information for
                            cybersecurity personnels to understand about the activities conducted by these threat
@@ -24,7 +24,7 @@ class TestResearchFieldClassifier(TestCase):
 
     def test_classify(self):
         top_n = 6
-        labels = self.classifier(abstract=self.abstract, top_n=top_n)
+        labels = self.classifier(raw_input=self.raw_input, top_n=top_n)
 
         self.assertIsInstance(labels, list)
         self.assertEqual(len(labels), top_n)
